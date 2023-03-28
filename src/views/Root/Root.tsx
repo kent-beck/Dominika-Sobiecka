a5f159b86b4d9b5eb19a9180c1974dec94e8f489
Dominika Sobiecka <dominika.sobiecka@gmail.com>
2023-03-28 18:22:42 +0200

    name

--- src/views/Root/Root.tsx
+++ src/views/Root/Root.tsx
@@ -4,7 +4,6 @@ import Welcome from "../../components/Welcome/Welcome";
 import DataTable from "../../components/DataTable/DataTable";
 import DataElement from "../../components/DataElement/DataElement";
 import Cart from '../../components/Cart/Cart';
-import BeerTitle from "../../components/UI/BeerTitle/BeerTitle";
 
 function Root() {
     return (
@@ -13,7 +12,7 @@ function Root() {
                 <Route path="/" element={<Welcome/>}/>
                 <Route path="/datatable" element={<DataTable/>}/>
                 <Route path='/dataelement/:beerId' element={<DataElement/>}/>
-                <Route path='/cart' element={<Cart/>}/>              
+                <Route path='/cart' element={<Cart/>}/>
             </Routes>
         </Router>
     );
